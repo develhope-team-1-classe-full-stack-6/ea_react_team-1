800cf00da9c77a1cb53702b0c8ee30c8321f6d17
Ugo Scelfo <ugoscelfo92@gmail.com>
2022-12-12 11:27:25 +0100

    box component basic v.1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { Routes, Route } from "react-router-dom";
+import BoxComponent from "./components/components/box/BoxComponent";
 
 export const App = () => {
   return (
+    <>
     <Routes>
       <Route path="/" element={<div>Home</div>}/>
     </Routes>
+    <BoxComponent />
+    </>
   );
 }
---
+++ src/components/components/box/BoxComponent.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import './BoxComponent.css';
+import fifa23 from '../../../assets/images/box/eas-fifa22-gen5-ue-keyart-horz-f23logo.jpg.adapt.crop1x1.767p.jpg';
+import fifa23Logo from '../../../assets/images/box/fifa23-logo-white-stacked.svg';
+
+export default function BoxComponent() {
+
+  return (
+    <Box className="box-container"
+      sx={{
+        width: 300,
+        height: 300,
+        backgroundColor: 'primary.dark',
+        '&:hover': {
+          backgroundColor: 'primary.main',
+          opacity: [0.9, 0.8, 0.7],
+        },
+      }}
+    >
+      <img className="box-background" src={fifa23} width="100%" height="100%" alt=""/>
+      <img className="box-logo" src={fifa23Logo} width="100%" height="100%" alt=""/>
+      <p>Sito ufficiale</p>
+  
+    </Box>
+  );
+}
