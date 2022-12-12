b1b356693dcc7546b8738fedaed684c21c7a411d
Filippo Di Marco <filidm5795@gmail.com>
2022-12-12 14:37:30 +0100

    fix app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 
-export const App = () => {
+export default function App (){
   return (
     <Routes>
       <Route path="/" element={<div>Home</div>}/>
