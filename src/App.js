514a764c97a4deff7510f88819dc34c041745147
d-94k <86233674+d-94k@users.noreply.github.com>
2022-12-12 15:33:05 +0100

    setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
+import Footer from "./components/layout/tab/Footer";
 
 export default function App (){
   return (
     <Routes>
       <Route path="/" element={<div>Home</div>}/>
+      <Route path="/footer" element={<Footer />} />
     </Routes>
   );
 }
---
+++ src/components/layout/tab/Footer.js
@@ -0,0 +1,11 @@
+import { Box } from '@mui/material';
+
+const Footer = () => {
+    return (
+        <Box>
+            Hello
+        </Box>
+    );
+}
+ 
+export default Footer;
