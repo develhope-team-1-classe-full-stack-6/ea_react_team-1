c7e877d9c39dfb720f48638c93889604eb5d503b
Filippo Di Marco <filidm5795@gmail.com>
2022-12-12 14:35:43 +0100

    fix app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import CustomButton from "./components/components/button/CustomButton";
 export const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<CustomButton></CustomButton>}/>
+      <Route path="/" element={<div>Home</div>}/>
     </Routes>
   );
 }
