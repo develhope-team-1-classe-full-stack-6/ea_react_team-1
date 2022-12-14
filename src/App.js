0d802fe25ce01430baabe388e12ac1ce5a873f28
mirko <autieromir@gmail.com>
2022-12-14 13:27:38 +0100

    commit backup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
+import { BannerFooter } from "./components/components/banner-footer/BannerFooter"
 
 export default function App (){
   return (
     <Routes>
       <Route path="/" element={<div>Home</div>}/>
+      <Route path="banner" element={<BannerFooter />} />
     </Routes>
   );
 }
