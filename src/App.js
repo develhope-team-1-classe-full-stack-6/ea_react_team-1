bd7aaefe47ee2435ceb71c2855ff516545e6e1b8
d-94k <86233674+d-94k@users.noreply.github.com>
2022-12-19 23:21:29 +0100

    merged from dev

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { Routes, Route } from "react-router-dom";
+import Footer from "./components/layout/Footer";
 
 export default function App (){
   return (
     <Routes>
-      <Route path="/" element={<div>Home</div>}/>
+      <Route path="/" element={<Footer />}/>
     </Routes>
   );
 }
