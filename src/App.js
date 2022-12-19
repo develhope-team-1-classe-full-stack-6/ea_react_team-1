d68145025c7bcce4e7e274035a63ddddbe194291
Filippo Di Marco <filidm5795@gmail.com>
2022-12-19 15:56:09 +0100

    correct App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import EANavbar from "./components/layout/navbar/EANavbar2";
+import { Routes, Route } from "react-router-dom";
 
 export default function App (){
   return (
-    // <Routes>
-    //   <Route path="/" element={<div>Home</div>}/>
-    // </Routes>
-    <EANavbar />
+    <Routes>
+      <Route path="/" element={<div>Home</div>}/>
+    </Routes>
   );
 }
