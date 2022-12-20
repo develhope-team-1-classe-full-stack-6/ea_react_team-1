7f7b7d197b567655848beb3b54a49c76d984809a
Ugo Scelfo <ugoscelfo92@gmail.com>
2022-12-20 20:04:31 +0100

    restore App file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-import EABox from "./components/components/box/EABox";
 
-const links = [
-  {
-    title: "Sito ufficiale",
-    link: "#"
-  },
-  {
-    title: "Aiuto",
-    link: "#"
-  },
-  {
-    title: "Forum",
-    link: "#"
-  }
-]
-
-export default function App (){
+export default function App() {
   return (
-    <>
+
     <Routes>
-      <Route path="/" element={<div>Home</div>}/>
+      <Route path="/" element={<div>Home</div>} />
     </Routes>
-    <EABox variant="up" overlay="" links={links} logoWidth="" />
-    </>
   );
 }
