d3f02609735e6a355e4e3b1c9fff14e3ead6fd2f
d-94k <86233674+d-94k@users.noreply.github.com>
2022-12-24 11:26:59 +0100

    imported otherfooter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Footer from "./components/layout/Footer";
+import Otherfooter from "./components/layout/Otherfooter";
 
 export default function App (){
   return (
     <Routes>
-      <Route path="/" element={<Footer />}/>
+      <Route path="/" element={<Otherfooter />}/>
     </Routes>
   );
 }
