572f3086db916015a3b3f883001741f367f08160
Ugo Scelfo <ugoscelfo92@gmail.com>
2022-12-29 19:11:25 +0100

    restored app file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-import BoxSection from "./components/pages/home/giochi-in-evidenza/BoxSection";
-
 
 export default function App() {
   return (
-    <BoxSection />
-    // <Routes>
-    //   <Route path="/" element={<div>Home</div>} />
-    // </Routes>
+
+    <Routes>
+      <Route path="/" element={<div>Home</div>} />
+    </Routes>
   );
 }
