b0c4477e2ce1ca44d09f0c3c0157d1fd1383d9f6
Ugo Scelfo <ugoscelfo92@gmail.com>
2022-12-30 16:54:58 +0100

    added boxes to aside and changed folder

--- src/components/layout/boxAside/BoxAside.js
+++ src/components/layout/boxAside/BoxAside.js
@@ -2,6 +2,10 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
+import { Grid } from "@mui/material";
+import { boxes } from "./data";
+import { Container } from "@mui/system";
+import { styled } from '@mui/material/styles';
 
 export default function BoxAside() {
   const [state, setState] = React.useState({
@@ -16,6 +20,31 @@ export default function BoxAside() {
     setState({ ...state, [anchor]: open }); 
   };
 
+  // Container styling
+  const BoxWrapper = styled(Container)(({ theme }) => ({
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    margin: "50px auto",
+    h3:{
+      ...theme.typography.d1,
+        fontSize: "40px",
+        marginBottom: "32px",
+        "@media (max-width: 599px)":{
+            fontSize: "32px",
+        },
+        "@media (max-width: 290px)":{
+            fontSize: "16px",
+        }
+    },
+    li:{
+        ...theme.typography.d1,
+        listStyle: "none",
+        fontSize: "18px"
+    }
+  }));
+
+  // Aside content
   const list = (anchor) => (
     <Box
       sx={{ width: 400 }}
@@ -25,9 +54,15 @@ export default function BoxAside() {
         <a href="#">Tutti i giochi</a>
         <Button onClick={toggleDrawer(anchor, false)}>X</Button>
       </header>
-      <main>
-
-      </main>
+      <BoxWrapper>
+        <Grid container spacing={4}>
+          {boxes.map((item) => (
+            <Grid item xs={6} sm={6} md={6}>
+              <li>{item.box}</li>
+            </Grid>
+          ))}
+        </Grid>
+      </BoxWrapper>
     </Box>
   );
 
