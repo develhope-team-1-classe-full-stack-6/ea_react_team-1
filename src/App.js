c179e8354383df9ebfbdee21810f8d49cff9ce05
Ugo Scelfo <ugoscelfo92@gmail.com>
2023-01-02 17:18:59 +0100

    restore app file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-import BoxAside from "./components/layout/boxAside/BoxAside";
 
 export default function App() {
   return (
-    <BoxAside />
-    // <Routes>
-    //   <Route path="/" element={<div>Home</div>} />
-    // </Routes>
+
+    <Routes>
+      <Route path="/" element={<div>Home</div>} />
+    </Routes>
   );
 }
