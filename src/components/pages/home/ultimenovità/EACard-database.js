23245e9f1f4d276f082b29d34d2e32e118a573e9
Dario <dariogalesi@gmail.com>
2023-01-03 11:00:46 +0100

    added images

---
+++ src/components/pages/home/ultimenovità/EACard-database.js
@@ -0,0 +1,19 @@
+import EACard from "../../../components/ea-card/EACard";
+
+const cards = [
+    {
+        id: 0,
+        card: 
+            <EACard
+                backgroundColor=''
+                img=''
+                alt=''
+                link='https://www.ea.com/news/year-in-gaming-2022'
+                info=''
+                date=''
+                titleColor=''
+                title=''
+                text=''
+            />
+    }
+]
