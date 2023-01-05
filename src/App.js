801ab1321f9d88f4efc9f1d0543253bf8381567f
hershellayton95 <filidm5795@gmail.com>
2023-01-05 17:40:28 +0100

    update nav1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-// import { Routes, Route } from "react-router-dom";
-import EANavbar1 from "./components/layout/navbar/EANavbar1";
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
 
-    // <Routes>
-    //   <Route path="/" element={<div>Home</div>} />
-    // </Routes>
-    <EANavbar1/>
+    <Routes>
+      <Route path="/" element={<div>Home</div>} />
+    </Routes>
   );
 }
