f3c66210acd3a96dc03a63262673f005c0194940
Dario Galesi <dariogalesi@gmail.com>
2023-01-06 11:57:50 +0100

    app fixed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import { Routes, Route } from "react-router-dom";
-import EACard from "./components/components/ea-card/EACard";
-import LatestGrid from "./components/pages/home/ultimenovità/LatestGrid";
 
 export default function App() {
-  return (
-    <LatestGrid/>   
-  //   <Routes>
-  //     <Route path="/" element={<div>Home</div>} />
-  //   </Routes>
+  return (   
+    <Routes>
+      <Route path="/" element={<div>Home</div>} />
+    </Routes>
   );
 }
