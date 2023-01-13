aa27f99e235f9d385651c35b08751308ba179285
Dario <dariogalesi@gmail.com>
2023-01-13 10:38:43 +0100

    grid fixed with json data fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/pages/home/Home";
+import LatestGrid from "./components/pages/home/ultimenovità/LatestGrid";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home/>} />
-    </Routes>
+    <LatestGrid/>
+    // <Routes>
+    //   <Route path="/" element={<Home/>} />
+    // </Routes>
   );
 }
--- src/components/pages/home/ultimenovità/LatestGrid.js
+++ src/components/pages/home/ultimenovità/LatestGrid.js
@@ -1,6 +1,7 @@
 import { Grid } from "@mui/material";
 import { Container } from "@mui/system";
-import { cards } from "./EACard-database";
+import CardsLatestDatabase from '../../../../data/CardsLatestDatabase.json'
+import EACard from "../../../components/ea-card/EACard";
 
 
 function LatestGrid(){
@@ -15,16 +16,24 @@ function LatestGrid(){
             }}
         >
             <Grid container spacing={0}>
-                {cards.map((item, index)=>
+                {CardsLatestDatabase.map((item, index)=>
                     (
-                    <Grid item
+                    <Grid key={item.id} item
                     xs={12}
                     sm={6}
                     md={4}
                     lg={4}
-                    key={index+item}
+                    
                 >
-                    <div>{item.card}</div>
+                    <EACard
+                        img={item.card.img}
+                        alt={item.card.alt}
+                        link={item.card.link}
+                        info={item.card.info}
+                        date={item.card.date}
+                        title={item.card.title}
+                        text={item.card.text}
+                    />
                 </Grid>
                     )
                 )}
