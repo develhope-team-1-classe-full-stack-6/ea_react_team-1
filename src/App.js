acf80b222021cffed06281d52eaef176280d7509
Dario Galesi <dariogalesi@gmail.com>
2023-01-13 16:41:19 +0100

    tab v1.0

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/pages/home/Home";
+import EATab from "./components/components/ea-tab-nav/EATab"
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home/>} />
-    </Routes>
+    <EATab/>
+    // <Routes>
+    //   <Route path="/" element={<Home/>} />
+    // </Routes>
   );
 }
