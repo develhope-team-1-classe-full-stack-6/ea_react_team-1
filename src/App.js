03fa58a558c29b2d396255a9074a4fbbbb7de2a5
hershellayton95 <filidm5795@gmail.com>
2023-01-13 17:36:04 +0100

    ea-origin

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
+import EAOriginals from "./components/pages/ea-originals/EAOriginals";
 import Home from "./components/pages/home/Home";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home/>} />
+      <Route path="/ea-originals" element={<EAOriginals/>}/>
     </Routes>
   );
 }
