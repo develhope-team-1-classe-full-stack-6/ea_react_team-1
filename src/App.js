4b29a1f49fc61d99ec04e099130cda0b3b49a941
mirko <autieromir@gmail.com>
2023-01-20 12:23:58 +0100

    console pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-// import { Routes, Route } from "react-router-dom";
-// import EAOriginals from "./components/pages/ea-originals/EAOriginals";
-// import ComingSoon from "./components/pages/games/coming-soon/ComingSoon";
-// import { Games } from "./components/pages/games/Games";
-// import LatestGames from "./components/pages/games/latest-games/LatestGames";
-// import Home from "./components/pages/home/Home";
-// import { Switch } from "./components/pages/games/library/Switch";
-// import { Xbox } from "./components/pages/games/library/Xbox";
-
-import { Mobile } from "./components/pages/games/library/Mobile";
+import { Routes, Route } from "react-router-dom";
+import EAOriginals from "./components/pages/ea-originals/EAOriginals";
+import ComingSoon from "./components/pages/games/coming-soon/ComingSoon";
+import { Games } from "./components/pages/games/Games";
+import LatestGames from "./components/pages/games/latest-games/LatestGames";
+import Home from "./components/pages/home/Home";
 
 export default function App() {
   return (
-    <Mobile />
-    // <Routes>
-    //   <Route path="/" element={<Home/>} />
-    //   <Route path="/ea-originals" element={<EAOriginals/>}/>
-    //   <Route path="/games" element={<Games />} >
-    //     <Route index element={<LatestGames />} />
-    //     <Route path="coming-soon" element={<ComingSoon />} />
-    //   </Route>
-    // </Routes>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/ea-originals" element={<EAOriginals />} />
+      <Route path="/games" element={<Games />}>
+        <Route index element={<LatestGames />} />
+        <Route path="coming-soon" element={<ComingSoon />} />
+      </Route>
+    </Routes>
   );
 }
