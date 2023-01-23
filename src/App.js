1917fc8ef9879482d012c2034b56137fd46bcdb1
Mirko <76716235+hgxxwon@users.noreply.github.com>
2023-01-23 17:37:18 +0100

    console (#48)
    * commit backup
    
    * backup
    
    * backup
    
    * backup
    
    * pagine ps5, xbox, switch, mobile
    
    * console pages
    
    * consoles

--- src/App.js
+++ src/App.js
@@ -4,18 +4,16 @@ import ComingSoon from "./components/pages/games/coming-soon/ComingSoon";
 import { Games } from "./components/pages/games/Games";
 import LatestGames from "./components/pages/games/latest-games/LatestGames";
 import Home from "./components/pages/home/Home";
-import Commitments from "./components/pages/commitments/Commitments";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/ea-originals" element={<EAOriginals/>}/>
-      <Route path="/games" element={<Games />} >
+      <Route path="/" element={<Home />} />
+      <Route path="/ea-originals" element={<EAOriginals />} />
+      <Route path="/games" element={<Games />}>
         <Route index element={<LatestGames />} />
         <Route path="coming-soon" element={<ComingSoon />} />
       </Route>
-      <Route path="/commitments" element={<Commitments />} />
     </Routes>
   )
   }
