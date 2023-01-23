596655eff1e5dab3a7ddff93a559986e1e3c8114
Ugo Scelfo <ugoscelfo92@gmail.com>
2023-01-23 11:50:06 +0100

    fix carousel

--- src/components/components/carousel/EACarousel.js
+++ src/components/components/carousel/EACarousel.js
@@ -1,52 +1,115 @@
-import React, { useState } from 'react';
-import data from '../../../data/carouselDatabase.json';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import slides from '../../../data/carouselDatabase.json';
+import { useState } from 'react';
+import "./carousel.scss";
+import Modal from '@mui/material/Modal';
 
-export default function EACarousel ({ slides }) {
+export default function EACarousel () {
   const [current, setCurrent] = useState(1);
-  const length = slides.length;
-
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  
   const nextSlide = () => {
-    setCurrent(current === length - 1 ? 1 : current + 1);
+    setCurrent(current === slides.length - 1 ? 1 : current + 1);
   };
 
   const prevSlide = () => {
-    setCurrent(current === 1 ? length - 1 : current - 1);
+    setCurrent(current === 1 ? slides.length - 1 : current - 1);
   };
 
   if (!Array.isArray(slides) || slides.length <= 1) {
     return null;
   }
 
-  return (
-    <div className='bg-dark'>
-      <h1 className="text-white d-flex justify-content-center mt-5">Trailer</h1>
-      <div className="container carousel slide mt-5 position-relative">
-        <div>
-          {data.map((item, index) => {
-            return (
-              <div
-                className={index === current ? 'slide active' : 'slide'}
-                key={index}
-              >
-              {index === current && (
-                <iframe src={item.src} title={item.title}></iframe>
-              )}
-              </div>
-            );
-          })}
-        </div>
-        <div className="carousel-btn-controls d-flex justify-content-cente mt-4">
-          <button className="carousel-control-prev carousel-control-prev-icon" type="button" onClick={prevSlide}/>
-          <span className="text-white">{current}/10</span>
-          <button className="carousel-control-next carousel-control-next-icon" type="button" onClick={nextSlide} />
+  const Modalstyle = {
+    position: 'absolute',
+    width: "100vw",
+    height: "100vh",
+    bgcolor: '#000',
+    paddingTop: "40px"
+  };
 
-          {/* open modal  */}
-          <button id="open-modal-button" type="button" className="btn-trigger-modal" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            <img src=".assets/images/pages/ea-originals/expanding-arrows.png" alt="" />
-          </button>
+  return(
+    <div className='bg-dark'>
+      <div className="carousel-wrapper">
+        <h1 className="text-white d-flex justify-content-center pt-5">Trailer</h1>
+        <div className="container carousel slide mt-5 position-relative">
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+            {slides.map((item, index) => {
+              return(
+                <div className={index === current ? 'slide active' : 'slide'} key={index}>
+                  {index === current && (
+                    <iframe 
+                      style={{ width: "100%", aspectRatio: 16/9 }} 
+                      src={item.src} 
+                      title={item.title}>
+                    </iframe>
+                  )}
+                </div>
+              );
+            })}
+            </div>
+          </div>
+          <div className="d-flex justify-content-center align-items-center mt-4">
+            <button className="carousel-control-prev" type="button" onClick={prevSlide}>
+              <span className="carousel-control-prev-icon"></span>
+            </button>
+            <span className="text-white">{current}/10</span>
+            <button className="carousel-control-next" type="button" onClick={nextSlide}>
+              <span className="carousel-control-next-icon"></span>
+            </button>
+            <button type="button" className="btn-trigger-modal" onClick={handleOpen}>
+              <img src="./assets/images/pages/ea-originals/expanding-arrows.png" alt="" />
+            </button>
+          </div>
         </div>
+
+        {/* Modal */}      
+        <div>
+        <Modal
+          sx={Modalstyle}
+          open={open}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+        <div className="carousel-wrapper">
+          <div className='modalHeader'>
+            <button className='closeBtnModal' onClick={handleClose}>X</button>
+          </div>
+          <div className="container carousel slide mt-5 position-relative">
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+              {slides.map((item, index) => {
+                return(
+                  <div className={index === current ? 'slide active' : 'slide'} key={index}>
+                    {index === current && (
+                      <iframe 
+                        style={{ width: "100%", aspectRatio: 16/9 }} 
+                        src={item.src} 
+                        title={item.title}>
+                      </iframe>
+                    )}
+                  </div>
+                );
+              })}
+              </div>
+            </div>
+            <div className="d-flex justify-content-center align-items-center mt-4">
+              <button className="carousel-control-prev" type="button" onClick={prevSlide}>
+                <span className="carousel-control-prev-icon"></span>
+              </button>
+              <span className="text-white">{current}/10</span>
+              <button className="carousel-control-next" type="button" onClick={nextSlide}>
+                <span className="carousel-control-next-icon"></span>
+              </button>
+            </div>
+          </div>
+        </div>
+        </Modal>
+      </div>
       </div>
     </div>
-  );
-};
+  )
+}
