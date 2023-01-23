4b690cbc1e65df559f4e8b26d28d5b96b5aa5827
hershellayton95 <filidm5795@gmail.com>
2023-01-23 18:52:18 +0100

    update

--- src/App.js
+++ src/App.js
@@ -4,18 +4,39 @@ import ComingSoon from "./components/pages/games/coming-soon/ComingSoon";
 import { Games } from "./components/pages/games/Games";
 import LatestGames from "./components/pages/games/latest-games/LatestGames";
 import Home from "./components/pages/home/Home";
-import SignIn from "./components/pages/signin/SignIn";
+import News from "./components/pages/home/ultimenovità/Tabs/News";
+import EAPlay from "./components/pages/home/ultimenovità/Tabs/EAPlay"
+import Fifa from "./components/pages/home/ultimenovità/Tabs/Fifa";
+import Commitments from "./components/pages/commitments/Commitments";
+import LibreriaDeiGiochiGrid from "./components/pages/games/library/LibreriaDeiGiochiGrid";
+import Generi from "./components/pages/games/library/Tabs/Generi";
+import ClassificazioneEtà from "./components/pages/games/library/Tabs/ClassificazioneEtà";
+import ModalitàDiGioco from "./components/pages/games/library/Tabs/ModalitàDiGioco";
+import Piattaforme from "./components/pages/games/library/Tabs/Piattaforme";
+
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home />}>
+        <Route index element={<News />} />
+        <Route path="ea-news" element={<News />} />
+        <Route path="ea-play" element={<EAPlay />} />
+        <Route path="fifa" element={<Fifa />} />
+      </Route>
       <Route path="/ea-originals" element={<EAOriginals />} />
-      <Route path="/games" element={<Games />}>
+      <Route path="/games" element={<Games />} >
         <Route index element={<LatestGames />} />
         <Route path="coming-soon" element={<ComingSoon />} />
+        <Route path="library" element={<LibreriaDeiGiochiGrid />}>
+          <Route index element={<Generi />} />
+          <Route path="genres" element={<Generi />} />
+          <Route path="platforms" element={<Piattaforme />} />
+          <Route path="age-ratings" element={<ClassificazioneEtà />} />
+          <Route path="play-modes" element={<ModalitàDiGioco />} />
+        </Route>
       </Route>
-      <Route path="/signin" element={<SignIn />} />
+      <Route path="/commitments" element={<Commitments />} />
     </Routes>
-  )
+  );
 }
