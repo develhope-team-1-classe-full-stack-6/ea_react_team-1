9e176dbd4a89e9489f5252b2e199c94e609c92c5
hershellayton95 <filidm5795@gmail.com>
2023-01-27 22:03:25 +0100

    login page

--- src/components/pages/login/Login.js
+++ src/components/pages/login/Login.js
@@ -1,6 +1,26 @@
-import { Container, Typography } from "@mui/material";
+import { Button, Container, styled, Typography } from "@mui/material";
 import HeadForm from "../../layout/headLoginSignupForm/HeadForm";
 import CredentialForm from "./forms/CredantialForm";
+
+const CustomButton = styled(Button)({
+    height: "50px",
+    fontSize: "1.2rem",
+    lineHeight: 1.5,
+    color: "white",
+    fontWeight: "bold",
+    border: "2px solid #235fe3"
+});
+
+const CustomTypography = styled(Typography)({
+    color: "white",
+    display: "flex",
+    justifyContent: "center",
+    marginBlock: "20px",
+    "&:hover": {
+        color: "#1976d2",
+        textDecoration: "underline"
+    }
+});
 
 function SignIn() {
 
@@ -18,6 +38,18 @@ function SignIn() {
                     <Typography variant="h5" color="white.main"><strong>Accedi al tuo account EA</strong></Typography>
                 </HeadForm>
                 <CredentialForm />
+                <CustomTypography>Hai dimenticato la password?</CustomTypography>
+
+                <CustomButton
+                    type='submit'
+                    variant="outlined"
+                    color="primary"
+                    size="large"
+                    fullWidth
+                    margin="normal"
+                >
+                    Crea Account
+                </CustomButton>
             </Container >
         </Container>
     );
