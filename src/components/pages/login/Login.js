eb2488fc7c8a14679f1895cb06304715c8f528c9
Filippo Di Marco <filidm5795@gmail.com>
2023-01-28 23:50:56 +0100

    update login page (#60)
    * fix button
    
    * create container
    
    * update navbar
    
    * update navbar
    
    * update navbar
    
    * create banner
    
    * fix navbar aside
    
    * fix hero
    
    * banner footer
    
    * fix close aside
    
    * data create
    
    * ea-origin
    
    * card coming soon
    
    * card coming soon
    
    * card coming soon
    
    * fix
    
    * baner
    
    * update
    
    * update
    
    * fix
    
    * signin
    
    * login
    
    * update
    
    * added context
    
    * added context
    
    * head form sign
    
    * routes
    
    * routes
    
    * login page
    
    * update login page

--- src/components/pages/login/Login.js
+++ src/components/pages/login/Login.js
@@ -1,23 +1,56 @@
-import { Container, Typography } from "@mui/material";
+import { Button, Container, styled, Typography } from "@mui/material";
 import HeadForm from "../../layout/headLoginSignupForm/HeadForm";
 import CredentialForm from "./forms/CredantialForm";
+
+const CustomButton = styled(Button)({
+    height: "50px",
+    fontSize: "1.2rem",
+    lineHeight: 1.5,
+    color: "white",
+    fontWeight: "bold",
+    border: "2px solid #235fe3"
+});
+
+const CustomTypography = styled(Typography)({
+    color: "white",
+    display: "flex",
+    justifyContent: "center",
+    marginBlock: "20px",
+    "&:hover": {
+        color: "#1976d2",
+        textDecoration: "underline"
+    }
+});
 
 function SignIn() {
 
     const pageStyle = {
         height: " 100vh",
-        backgroundColor: "#1d2033"
+        backgroundColor: "#1d2033",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
     }
 
     return (
         <Container maxWidth="100vw" style={pageStyle}>
-            <Container maxWidth="xs"
-            // style={{ background: "white" }}
-            >
+            <Container maxWidth="xs" style={{ marginBottom: "70px" }}>
                 <HeadForm>
                     <Typography variant="h5" color="white.main"><strong>Accedi al tuo account EA</strong></Typography>
                 </HeadForm>
                 <CredentialForm />
+                <CustomTypography>Hai dimenticato la password?</CustomTypography>
+
+                <CustomButton
+                    type='submit'
+                    variant="outlined"
+                    color="primary"
+                    size="large"
+                    fullWidth
+                    margin="normal"
+                >
+                    Crea Account
+                </CustomButton>
             </Container >
         </Container>
     );
--- src/components/pages/login/forms/CredantialForm.js
+++ src/components/pages/login/forms/CredantialForm.js
@@ -1,8 +1,22 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Button, Checkbox, FormControlLabel, IconButton, InputAdornment, styled, TextField, Typography } from '@mui/material';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+
+const Loginutton = styled(Button)({
+    height: "45px",
+    fontSize: "1.2rem",
+    lineHeight: 1.5,
+    color: "white",
+    fontWeight: "bold"
+});
 
 const CredentialForm = () => {
+    require("./form.scss");
 
+    const [checkboxColor, setCheckboxColor] = useState(true);
+    const [showPassword, setShowPassword] = useState(false);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
@@ -29,24 +43,60 @@ const CredentialForm = () => {
 
     return (
         <form onSubmit={handleSubmit}>
-            <label>
-                Email:
-                <input
-                    type='text'
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-            </label>
-            <label>
-                Password:
-                <input
-                    type='password'
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-            </label>
-            <button type='submit'>Submit</button>
+            <Typography color="white.main" variant="body2"><strong>E-MAIL</strong></Typography>
+            <TextField
+                type="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                fullWidth
+                margin="normal"
+                variant="outlined"
+                placeholder='Inserisci il tuo indirizzo e-mail'
+            />
+            <Typography color="white.main" variant="body2"><strong>PASSWORD</strong></Typography>
+            <TextField
+                type={showPassword ? 'text' : 'password'}
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                fullWidth
+                margin="normal"
+                variant="outlined"
+                placeholder='Password'
+                InputProps={{
+                    endAdornment: (
+                        <InputAdornment position="end">
+                            <IconButton
+                                onClick={() => setShowPassword(!showPassword)}
+                                style={{ width: "40px", height: "30px", backgroundColor: "#242635", borderRadius: "5px" }}
+                            >
+                                {showPassword ? <VisibilityOffIcon
+                                    style={{ color: 'white', transform: "scale(0.7, 0.7)" }} /> : <VisibilityIcon
+                                    style={{ color: 'white', transform: "scale(0.7, 0.7)" }} />}
+                            </IconButton>
+                        </InputAdornment>
+                    ),
+                }}
+            />
+            <FormControlLabel
+                control={<Checkbox name="remember" color="primary" style={{ color: checkboxColor ? 'white' : "#235fe3" }} />}
+                onClick={() => { setCheckboxColor(b => !b) }}
+                label="Ricordami"
+                style={{ color: 'white' }}
+            />
+            <Loginutton
+                type='submit'
+                variant="contained"
+                size="large"
+                color="primary"
+                fullWidth
+                margin="normal"
+            >
+                Accedi
+            </Loginutton>
         </form>
+
     );
 };
 
